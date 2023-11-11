a959d2c9cd2e9ade2eda2213d6d02e60037acd8a
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-11-12 07:52:35 +1100

    get two numbers and add them together

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,13 +6,15 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean inputIsValid = false;
-        Double input = null;
+        Double inputOne = null;
+        Double inputTwo = null;
 
+//      Get first input
         do {
             System.out.print("Enter a number: ");
             String inputStr = scanner.nextLine();
             try {
-                input = Double.parseDouble(inputStr);
+                inputOne = Double.parseDouble(inputStr);
                 inputIsValid = true;
             } catch (NumberFormatException nfr) {
                 System.out.println("Input was not number, please enter a number!");
@@ -20,7 +22,24 @@ public class Calculator {
         }
 
         while (!inputIsValid);
-        System.out.println("You entered: " + input);
+        System.out.println("You entered: " + inputOne);
 
+//      Get second input
+        inputIsValid = false;
+        do {
+            System.out.print("Enter a number: ");
+            String inputStr = scanner.nextLine();
+            try {
+                inputTwo = Double.parseDouble(inputStr);
+                inputIsValid = true;
+            } catch (NumberFormatException nfr) {
+                System.out.println("Input was not number, please enter a number!");
+            }
+        }
+        while (!inputIsValid);
+        System.out.println("You entered: " + inputTwo);
+
+        double result = inputOne + inputTwo;
+        System.out.println("The sum of the two numbers is " + result);
     }
 }
