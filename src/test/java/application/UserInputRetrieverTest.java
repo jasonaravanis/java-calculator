35e17c8358c9067ff80f0b854c27165b30c1c4e2
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-11-12 13:02:50 +1100

    more tests

---
+++ src/test/java/application/UserInputRetrieverTest.java
@@ -0,0 +1,45 @@
+package application;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.*;
+
+class UserInputRetrieverTest {
+
+    private ByteArrayOutputStream testOut;
+
+    private final InputStream originalSystemIn = System.in;
+    private final PrintStream originalSystemOut = System.out;
+
+    @BeforeEach
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setIn(originalSystemIn);
+        System.setOut(originalSystemOut);
+    }
+    @Test
+    void getDoubleFromUser() {
+        String testString = "10\n";
+        provideInput(testString);
+        UserInputRetriever.getDoubleFromUser();
+        assertEquals("\nEnter a number: \nYou entered: 10.0\n", getOutput());
+    }
+}
