be01e4d1c027fc5e642d008c24f194f5a54a056c
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-11-12 11:37:03 +1100

    formatting

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,18 +2,12 @@ package app;
 
 public class Application {
     /*
-    * The application has a calculator class which takes in input parameters and returns  calculated values
-    *
+    * The application has a calculator class which takes in input parameters and returns calculated values
     * The application has a UserInputRetriever which handles retrieving user input
-    *
-    * The application starts by initialising a Calculator and a UserInputRetriever
+    * The application starts by initialising a Calculator and a UserInputRetriever (UIR)
     * The UIR will pause execution until the user enters something via the CLI
-    *
     * The UIR has no awareness of what needs to be inputted. It just waits to receive and pass on some input
-    *
-    *
-    *
-    *  */
+    * */
 
     public static void main(String[] args) {
         Double inputA = UserInputRetriever.getDoubleFromUser();
